ce1afcff693bf6fb42e5e82578ff489a6b5df14b
sanjeetboora <sanjeet.boora97@gmail.com>
2023-04-19 22:17:51 +0530

    mongo db uri added

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
-const mongoDbUri = 'mongodb://127.0.0.1:27017/crmApp';
-
-module.exports = mongoDbUri;
+const mongoDbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crmApp';
+const dbName = crmapp - db;
+module.exports = { mongoDbUri, dbName };
--- models/ticket.model.js
+++ models/ticket.model.js
@@ -1,49 +1,49 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const ticketSchema = new Schema({
     title: {
         type: String,
         required: true,
     },
-    description:{
+    description: {
         type: String,
         required: true,
     },
-    ticketPriority:{
+    ticketPriority: {
         type: Number,
         require: true,
         default: 4
     },
-    status:{
+    status: {
         type: String,
         required: true,
         default: "open",
         enum: ["open", "inProgress", "resolved", "onHold", "cancelled"],
     },
-    assignee:{ //email of the user who is assigning the ticket
+    assignee: { //email of the user who is assigning the ticket
         type: String,
     },
-    assignedTo:{//email of the user to whom the ticket is assigned
+    assignedTo: {//email of the user to whom the ticket is assigned
         type: String,
     },
-    clientName:{ //company's name by/for which this ticket is created
-        type: String,
-        required: true
-    },
-    createdBy:{ //email of the user who created this ticket
+    clientName: { //company's name by/for which this ticket is created
         type: String,
         required: true
     },
-    createdAt:{
+    createdBy: { //email of the user who created this ticket
+        type: String,
+        required: true
+    },
+    createdAt: {
         type: Date,
         immutable: true,
         default: Date.now,
     },
-    updatedAt:{
+    updatedAt: {
         type: Date,
-        default:Date.now,
-   },
+        default: Date.now,
+    },
 })
 
 module.exports = mongoose.model("Ticket", ticketSchema);
